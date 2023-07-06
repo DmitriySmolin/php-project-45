5ff17b8b4121374a9bcbe5c068dd86ad1d47ec57
neox56 <neox56@gmail.com>
2023-07-06 20:34:46 +0400

    refactor Even.php

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -8,16 +8,14 @@ use const BrainGames\Engine\ROUNDS;
 
 function even(): void
 {
+    $question = 'Answer "yes" if the number is even, otherwise answer "no".';
     $data = [];
-    $count = 0;
 
-    while ($count < ROUNDS) {
+    for ($i = 0; $i < ROUNDS; $i += 1) {
         $num = rand(1, 100);
         $data[$num] = isEven($num) ? 'yes' : 'no';
-        $count += 1;
     }
-    $task = 'Answer "yes" if the number is even, otherwise answer "no".';
-    engine($data, $task);
+    engine($data, $question);
 }
 
 function isEven(int $num): bool
